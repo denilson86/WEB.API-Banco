de3e5b3b0a27a3df6a07cfdc938208c1cc6ceb25
Isabela <Isabela@ISABELA>
2021-09-19 14:48:33 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ContaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB.API_Banco.Controllers
+{
+    public class ContaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ContaBancaria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB.API_Banco.Models
+{
+    public class ContaBancaria
+    {
+        public int idCounta { get; set; }
+        private double saldo { get; set; }
+    }
+}
